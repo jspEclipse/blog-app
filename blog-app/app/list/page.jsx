06151dac5bb5b176875b921cafda7e0c19112539
jspEclipse <96797930+jspEclipse@users.noreply.github.com>
2023-11-17 00:42:25 +0000

    adding blog stuff

---
+++ blog-app/app/list/page.jsx
@@ -0,0 +1,5 @@
+export default function ListPage() {
+    return (
+        <div>This is a List Page</div>
+    )
+}
---
+++ blog-app/app/view/page.jsx
@@ -0,0 +1,5 @@
+export default function ViewPage() {
+    return (
+        <div>This is a View Page</div>
+    )
+}
